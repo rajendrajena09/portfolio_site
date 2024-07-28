4ffa8687c8a1feaf60814fd227725caf099d9c9e
Rajendra jena <rajendrajena898@gmail.com>
2024-07-28 12:20:17 +0530

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // base: "/My_portfolio/",
+  base: "/portfolio_site/",
   css:{
     modules:{
       localsConvention:"camelCase",
